166c04aa77f17324a14008dac4d74553ad37a43a
“takato15” <gshawks21@gmail.com>
2022-01-28 15:51:59 +0000

    [fix]評価星表示

--- db/migrate/20220127164132_create_tohokus.rb
+++ db/migrate/20220127164132_create_tohokus.rb
@@ -2,7 +2,10 @@ class CreateTohokus < ActiveRecord::Migration[5.2]
   def change
     create_table :tohokus do |t|
       
-      
+      t.string :prefecture_name
+      t.string :title
+      t.text :caption
+      t.float :evaluation
 
       t.timestamps
     end
