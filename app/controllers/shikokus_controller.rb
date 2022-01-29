8fd3941e9f4eac69ae3e516db5d6e2c3fc2bbf0f
“takato15” <gshawks21@gmail.com>
2022-01-29 20:43:57 +0000

    [add]四国地方ビューページ作成

--- app/controllers/shikokus_controller.rb
+++ app/controllers/shikokus_controller.rb
@@ -1,2 +1,27 @@
 class ShikokusController < ApplicationController
+  
+  def index
+    @shikokus = Shikoku.all.order(created_at: :desc).page(params[:page]).per(10)
+    @shikokus_all = Shikoku.all
+  end
+  
+  def new
+    @shikoku = Shikoku.new
+  end
+  
+  def create
+    @shikoku = Shikoku.new(shikoku_params)
+    if @shikoku.save
+      redirect_to shikokus_path
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def shikoku_params
+    params.require(:shikoku).permit(:title, :caption, :evaluation, :prefecture_name)
+  end
+  
 end
--- app/models/shikoku.rb
+++ app/models/shikoku.rb
@@ -5,4 +5,12 @@ class Shikoku < ApplicationRecord
     tokushima: 0, kagawa: 1, ehime: 2, kochi: 3
   }
   
+  # バリデーション
+  with_options presence: true do
+    validates :prefecture_name
+    validates :title
+    validates :evaluation
+    validates :caption
+  end
+  
 end
